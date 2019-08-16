918820fce8d1f2b00fc87825f2414419ba5aac34
Na Hyung Choi <schoinh@gmail.com>
2019-08-16 13:22:22 -0700

    delete account index view

--- SweetSavory/Controllers/AccountController.cs
+++ SweetSavory/Controllers/AccountController.cs
@@ -21,11 +21,6 @@ namespace SweetSavory.Controllers
             _db = db;
         }
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Register()
         {
             return View();
